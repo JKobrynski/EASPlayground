6d1edfb497c5a886ddb01e144ca33c77c454c5e9
Julian Kobrynski <julian.kobrynski@netguru.com>
2022-06-18 15:32:14 +0200

    Dev splash screen added

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ export default {
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
   splash: {
-    image: "./assets/splash.png",
+    image: IS_DEV ? "./assets/splash-dev.png" : "./assets/splash.png",
     resizeMode: "contain",
     backgroundColor: "#ffffff",
   },
