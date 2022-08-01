f4267e1d8c2f25282e4f12a72c548a5cfc7ed76d
Julian Kobrynski <julian.kobrynski@netguru.com>
2022-08-01 15:56:28 +0200

    old name brought back

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ const IS_DEV = process.env.APP_VARIANT === "development"
 
 export default {
   name: IS_DEV ? "Splash(dev)" : "Splash",
-  slug: "Splash",
+  slug: "EASTest",
   version: "1.0.0",
   orientation: "portrait",
   icon: IS_DEV ? "./assets/icon-dev.png" : "./assets/icon-demo.png",
@@ -20,14 +20,14 @@ export default {
   assetBundlePatterns: ["**/*"],
   ios: {
     supportsTablet: true,
-    bundleIdentifier: IS_DEV ? "com.jkobrynski.Splash.dev" : "com.jkobrynski.Splash",
+    bundleIdentifier: IS_DEV ? "com.jkobrynski.EASTest.dev" : "com.jkobrynski.EASTest",
   },
   android: {
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#FFFFFF",
     },
-    package: IS_DEV ? "com.jkobrynski.Splash.dev" : "com.jkobrynski.Splash",
+    package: IS_DEV ? "com.jkobrynski.EASTest.dev" : "com.jkobrynski.EASTest",
   },
   web: {
     favicon: "./assets/favicon.png",
